cd077f246397f9c4ff6e865a07e34664ba3bd754
maxjnorman <maxjnorman@gmail.com>
2020-06-02 13:01:11 +0100

    Minor change to main.py
    Pretty much just formatting

--- genetic-algorithm-feature-selection/main.py
+++ genetic-algorithm-feature-selection/main.py
@@ -7,8 +7,10 @@ from modules.clade import Clade, Individual
 root = Clade(initial_descendants=[Individual(),Individual(),Individual(),
                                   Individual(), Individual()],
              n_max=8, max_daughters=2)
-vine = Clade(initial_descendants=[
-    Clade(initial_descendants=[
-        Clade(initial_descendants=[
-            Individual()
-            ])])])
+vine = Clade(
+    initial_descendants=[Clade(
+        initial_descendants=[Clade(
+            initial_descendants=[Individual()]
+            )]
+        )]
+    )
