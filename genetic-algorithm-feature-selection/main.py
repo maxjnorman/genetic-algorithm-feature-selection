3ceaf918e71c93fdfc27c9ab1e5f59478b9c7724
maxjnorman <maxjnorman@gmail.com>
2020-06-15 17:33:53 +0100

    Implementing competition between descs

--- genetic-algorithm-feature-selection/main.py
+++ genetic-algorithm-feature-selection/main.py
@@ -5,82 +5,113 @@ from modules.functions import chain
 from modules.clade import Clade, Individual
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
 
 max_daughters = 8
 
-clade = Clade(
-    y="A",
-    max_daughters=max_daughters,
-    initial_descendants=[
+# clade = Clade(
+#     y="A",
+#     max_daughters=max_daughters,
+#     initial_descendants=[
 
-        Individual(y=0),
+#         Individual(y=0),
 
-        Individual(y=1),
+#         Individual(y=1),
 
-        Clade(
-            y="B",
-            max_daughters=max_daughters,
-            initial_descendants=[
+#         Clade(
+#             y="B",
+#             max_daughters=max_daughters,
+#             initial_descendants=[
 
-                Individual(y=2),
+#                 Individual(y=2),
 
-                Individual(y=3),
+#                 Individual(y=3),
 
-                Individual(y=4)
-            ]),
+#                 Individual(y=4)
+#             ]),
 
-        Clade(
-            y="C",
-            max_daughters=max_daughters,
-            initial_descendants=[
+#         Clade(
+#             y="C",
+#             max_daughters=max_daughters,
+#             initial_descendants=[
 
-                Individual(y=5),
+#                 Individual(y=5),
 
-                Clade(
-                    y="D",
-                    max_daughters=max_daughters,
-                    initial_descendants=[
+#                 Clade(
+#                     y="D",
+#                     max_daughters=max_daughters,
+#                     initial_descendants=[
 
-                        Individual(y=6),
+#                         Individual(y=6),
 
-                        Clade(
-                            y="E",
-                            max_daughters=max_daughters,
-                            initial_descendants=[
+#                         Clade(
+#                             y="E",
+#                             max_daughters=max_daughters,
+#                             initial_descendants=[
 
-                                Individual(y=7),
+#                                 Individual(y=7),
 
-                                Individual(y=8),
+#                                 Individual(y=8),
 
-                                Individual(y=9)
+#                                 Individual(y=9)
 
-                            ])
-                    ])
-            ])
+#                             ])
+#                     ])
+#             ])
 
-    ])
+#     ])
 tree = DecisionTreeClassifier
 root = Clade(
     initial_descendants=[
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree()),
-        Individual(model=tree()),Individual(model=tree()),Individual(model=tree())],
-    n_max=8, max_daughters=max_daughters
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),
+        Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3))],
+    n_max=128, max_daughters=max_daughters, train_fraction=.75
     )
-vine = Clade(
-    initial_descendants=[Clade(
-        initial_descendants=[Clade(
-            initial_descendants=[Individual(model=tree())]
-            )]
-        )]
-    )
+# vine = Clade(
+#     initial_descendants=[Clade(
+#         initial_descendants=[Clade(
+#             initial_descendants=[Individual(model=tree(max_depth=3))]
+#             )]
+#         )]
+#     )
+
+
+dat = pd.read_csv("/Users/maxjnorman/OneDrive - HEOR Ltd/Projects/Main/HEOR00324 - ML INR/dataset_20/baseline_20_simple.csv")
+y = (dat["ttr_out_ros"] > 0.3).astype(int).values
+X = dat.loc[:,dat.columns != "ttr_out_ros"].values
+
+root.fit(X,y)
+
+
+
+train_fraction = .75
+_model = tree(max_depth=3)
+
+n = y.shape[0]
+_idx = np.random.choice(
+            np.arange(n),
+            int(np.ceil(n * train_fraction)),
+            replace=False
+        )
+mask = np.repeat(False, n)
+np.put(mask, _idx, True)
+_mask = mask
+_X = X
+_y = y
+_model.fit(_X[_mask], _y[_mask])
+_oob_score = _model.predict_proba(_X[np.logical_not(_mask)])[:,1]
+_oob_truth = _y[np.logical_not(_mask)]
+_oob = _oob_score - _oob_truth
+
+
--- genetic-algorithm-feature-selection/modules/clade.py
+++ genetic-algorithm-feature-selection/modules/clade.py
@@ -6,7 +6,8 @@ from .functions import chain
 class CladeBase:
 
     def __init__(self, X=None, y=None, model=None, initial_descendants=[],
-                 n_max=8, n_decay_factor=.5, max_daughters=2, **kwargs):
+                 n_max=8, n_decay_factor=.5, max_daughters=2,
+                 train_fraction=.666667, oob_power=2., **kwargs):
         self._descs = initial_descendants
         self.gen = 0
         self._n_max = n_max # living individuals
@@ -15,6 +16,8 @@ class CladeBase:
         self._y = y
         self._model = model  # only to be used to build individuals
         self._oob = None
+        self._train_fraction = train_fraction
+        self._oob_power = oob_power
 
     @property
     def alive(self):
@@ -100,16 +103,28 @@ class CladeBase:
             # print("tally: {}".format(tally))
         return part
 
+    def compete(self, iter, loser=False):
+        teams = self.part(iter, n=2)
+        red, blue = [np.nanmean(np.stack([i.error for i in team]), axis=0)
+                     for team
+                     in teams]
+        score = np.nanmean(red - blue)
+        if score > 0:
+            teams = teams[::-1]
+        return teams
+
+
+
     def _champ(self, loser=False):
         stable = list(self.descs)
-        while len(stable) > 1:
-            arena = self.comp(self.part(stable, 2))
-        #arena sorts by shared mse
-        if loser is true:
-            stable = arena[-1]
-        else:
-            stable=arena[0]
         if len(stable) > 0:
+            while len(stable) > 1:
+                arena = None # TODO:
+                #arena sorts by shared mse
+                if loser is True:
+                    stable = arena[0]
+                else:
+                    stable = arena[-1]
             champ = stable[0]
         else:
             champ = None
@@ -135,13 +150,6 @@ class CladeBase:
 
     def kill(self):
         self.baggage().kill()
-
-    @property
-    def oob(self):
-        oob = np.repeat(np.nan, self.y.shape[0])
-        if self._oob is not None:
-            put(oob, self._oob[0], self._oob[1])
-        return oob
 
     @property
     def clade(self):
@@ -174,24 +182,41 @@ class CladeBase:
             prediction = desc.predict(X, check_input=True)
             if prediction is not None:
                 if len(prediction.shape) == 1:
-                # try:
                     stack.append(prediction)
-                # except:
-                #     pass
                 elif len(prediction.shape) == 2:
-                # try:
                     concat.append(prediction)
-                # except:
-                    # pass
                 else:
                     pass
         predictions = []
-        # if len(stack) > 0:
         try:
             predictions.append(np.stack(stack, axis=0))
         except:
             pass
-        # if len(concat) > 0:
+        try:
+            predictions.append(np.concatenate(concat, axis=0))
+        except:
+            pass
+
+        if len(predictions) > 0:
+            return np.concatenate(predictions, axis=0)
+
+    def predict_proba(self, X, check_input=True):
+        stack = []
+        concat = []
+        for desc in self.descs:
+            prediction = desc.predict_proba(X, check_input=True)
+            if prediction is not None:
+                if len(prediction.shape) == 2:
+                    stack.append(prediction)
+                elif len(prediction.shape) == 3:
+                    concat.append(prediction)
+                else:
+                    pass
+        predictions = []
+        try:
+            predictions.append(np.stack(stack, axis=0))
+        except:
+            pass
         try:
             predictions.append(np.concatenate(concat, axis=0))
         except:
@@ -204,7 +229,8 @@ class CladeBase:
 class Clade(CladeBase):
 
     def __init__(self, X=None, y=None, model=None, initial_descendants=[], n=8,
-                 max_daughters=2, **kwargs):
+                 max_daughters=2,
+                 train_fraction=.666667, oob_power=2., **kwargs):
         super().__init__(**kwargs)
         self._descs = initial_descendants
         self.gen = 0
@@ -214,6 +240,8 @@ class Clade(CladeBase):
         self._y = y
         self._model = model  # only to be used to build individuals
         self._oob = None
+        self._train_fraction = train_fraction
+        self._oob_power = oob_power
 
     @property
     def len_max(self):
@@ -237,31 +265,78 @@ class Clade(CladeBase):
             oob = desc.oob
             if oob is not None:
                 if len(oob.shape) == 1:
-                # try:
                     stack.append(oob)
-                # except:
-                #     pass
                 elif len(oob.shape) == 2:
-                # try:
                     concat.append(oob)
-                # except:
-                    # pass
                 else:
                     pass
         oobs = []
-        # if len(stack) > 0:
         try:
             oobs.append(np.stack(stack, axis=0))
-        except:
+        except: # TODO: catch specific exception
             pass
-        # if len(concat) > 0:
         try:
             oobs.append(np.concatenate(concat, axis=0))
-        except:
+        except: # TODO: catch specific exception
             pass
-
         if len(oobs) > 0:
             return np.concatenate(oobs, axis=0)
+        else:
+            return None
+
+    @property
+    def error(self):
+        stack = []
+        concat = []
+        for desc in self.descs:
+            error = desc.error
+            if error is not None:
+                if len(error.shape) == 1:
+                    stack.append(error)
+                elif len(error.shape) == 2:
+                    concat.append(error)
+                else:
+                    pass
+        errors = []
+        try:
+            errors.append(np.stack(stack, axis=0))
+        except: # TODO: catch specific exception
+            pass
+        try:
+            errors.append(np.concatenate(concat, axis=0))
+        except: # TODO: catch specific exception
+            pass
+        if len(errors) > 0:
+            return np.concatenate(errors, axis=0)
+        else:
+            return None
+
+    @property
+    def genotype(self):
+        stack = []
+        concat = []
+        for desc in self.descs:
+            genotype = desc.genotype
+            if genotype is not None:
+                if len(genotype.shape) == 1:
+                    stack.append(genotype)
+                elif len(genotype.shape) == 2:
+                    concat.append(genotype)
+                else:
+                    pass
+        genotypes = []
+        try:
+            genotypes.append(np.stack(stack, axis=0))
+        except: # TODO: catch specific exception
+            pass
+        try:
+            genotypes.append(np.concatenate(concat, axis=0))
+        except: # TODO: catch specific exception
+            pass
+        if len(genotypes) > 0:
+            return np.concatenate(genotypes, axis=0)
+        else:
+            return None
 
 
     def branch(self):
@@ -269,15 +344,14 @@ class Clade(CladeBase):
         n = np.min([n, self.len_max])
         n = int(n)
         if n > 1:
-            # print("n: {}".format(n))
             part_descs = self.part(self.descs)
-            # print("part_descs: {}".format(part_descs))
             clades = []
             for part_desc in part_descs:
                 clades.append(Clade(X=self.X, y=self.y, model=self.model,
                               n_max=self.n_max * self.n_decay_factor,
                               initial_descendants=part_desc,
-                              n_decay_factor=self.n_decay_factor))
+                              n_decay_factor=self.n_decay_factor,
+                              max_daughters=self._len_max))
             self._descs = clades
         self._branch_descs()
 
@@ -285,10 +359,8 @@ class Clade(CladeBase):
         """
         want to remove clades with single descendants
         """
-        # print("len(list(self.descs)): {}".format(len(list(self.descs))))
         for desc in self.descs:
             desc.collapse()
-            # print("len(list(desc.descs)): {}".format(len(list(desc.descs))))
             if desc._len_descs() == 1:
                 self._descs = list(self.descs) + list(desc.descs)
                 desc._descs = [] # this kills the clade
@@ -309,61 +381,40 @@ class Clade(CladeBase):
         """
         tidy = []
         untidy = []
-        # print()
-        # print([self])
         for desc in self.descs:
             desc.simplify()
-            # print("desc._len_descs(): {}".format(desc._len_descs()))
-            # print("desc.len_max: {}".format(desc.len_max))
             if desc._len_descs() < desc.len_max:
                 untidy.append(desc)
             else:
                 tidy.append(desc)
-        # print("tidy: {}".format(tidy))
-        # print("len(tidy): {}".format(len(tidy)))
-        # print("len(untidy): {}".format(len(untidy)))
         if len(untidy) == 1:
             tidy = tidy + untidy
             untidy = []
         elif len(untidy) > 1:
             lens = [d._len_descs() for d in untidy]
-            # print("lens: {}".format(lens))
             key = np.argsort(lens)
             key = key.max() - key
-            # print("key: {}".format(key))
             untidy = np.array(untidy)[key]
-            # print("untidy: {}".format(untidy))
             lens = np.array(lens)[key]
-            # print("lens: {}".format(lens))
             for i, desc in enumerate(untidy):
                 idx = np.arange(0, untidy.shape[0])
                 idx_i = idx[idx != i]
                 lens_i = lens[idx_i]
                 untidy_i = untidy[idx_i]
                 while (len(lens_i) > 0) and (np.min(lens_i) + desc._len_descs() < desc.len_max):
-                        # print("len(lens_i): {}".format(len(lens_i)))
-                        # print("np.min(lens_i): {}".format(np.min(lens_i)))
-                        # print("desc._len_descs(): {}".format(desc._len_descs()))
-                        # print("desc.len_max: {}".format(desc.len_max))
-                        # print("lens_i: {}".format(lens_i))
-                        # print("untidy_i: {}".format(untidy_i))
-                        # print()
-                        idx_argmin = np.argmin(lens_i)
-                        problem = untidy_i[idx_argmin]
-                        desc._descs = list(desc.descs) + list(problem.descs)
-                        problem._descs = []
-                        # try:
-                        untidy_i = np.delete(untidy_i, idx_argmin)
-                        lens_i = np.delete(lens_i, idx_argmin)
-                        # except:
-                        #     untidy_i = []
-                        #     lens_i = []
+                    idx_argmin = np.argmin(lens_i)
+                    problem = untidy_i[idx_argmin]
+                    desc._descs = list(desc.descs) + list(problem.descs)
+                    problem._descs = []
+                    untidy_i = np.delete(untidy_i, idx_argmin)
+                    lens_i = np.delete(lens_i, idx_argmin)
 
 
 
 class Individual(CladeBase):
 
-    def __init__(self, X=None, y=None, model=None, genes=None, train_fraction=.5, oob_power=2., **kwargs):
+    def __init__(self, X=None, y=None, model=None, genes=None,
+                 train_fraction=.666667, oob_power=2., **kwargs):
         super().__init__(**kwargs)
         self.gen = 0
         self.n_max = 0 # allow many offspring...
@@ -374,12 +425,26 @@ class Individual(CladeBase):
         self._y = y
         self._model = model
         self._oob = None
+        self._oob_score = None
+        self._oob_truth = None
         self._mask = None
         self._idx = None
         self._alive = True
         self._fertile = False
         self._train_fraction = train_fraction
         self._oob_power = oob_power
+        self._genes = genes
+
+    @property
+    def train_fraction(self):
+        return self._train_fraction
+
+    @train_fraction.setter
+    def train_fraction(self, train_fraction):
+        assert(isinstance(train_fraction, float))
+        assert(train_fraction <= 1.)
+        assert(train_fraction >= 0.)
+        self._train_fraction = train_fraction
 
     @property
     def len_max(self):
@@ -404,38 +469,59 @@ class Individual(CladeBase):
         return [self]
 
     @property
-    def oob(self):
+    def error(self):
         if self._oob is None:
             return None
         else:
             oob = np.repeat(np.nan, self._mask.shape[0])
             np.put(oob, self._idx, self._oob)
+            oob = np.abs(oob)
+            oob = np.power(oob, self._oob_power)
             return oob
 
+    @property
+    def oob(self):
+        if self._oob is None:
+            return None
+        else:
+            mask = np.logical_not(self._mask)
+            oob = np.repeat(np.nan, mask.shape[0])
+            idx = np.where(mask)[0]
+            np.put(oob, idx, self._oob_score)
+            return oob
+
+
     def champ(self, **kwargs):
-        return self.clade
+        return self
 
     def kill(self):
         self._alive = False
 
-    def fit(self, X, y, sample_weight=None, check_input=True, X_idx_sorted=None):
+    def fit(self, X, y, sample_weight=None, check_input=True,
+            X_idx_sorted=None):
         n = y.shape[0]
         # TODO: split into train and test and bootstrap
-        self._idx = np.random.choice(np.arange(n), int(np.ceil(n * self._train_fraction)), replace=False)
+        self._idx = np.random.choice(
+            np.arange(n),
+            int(np.ceil(n * self.train_fraction)),
+            replace=False
+        )
         mask = np.repeat(False, n)
         np.put(mask, self._idx, True)
         self._mask = mask
-        # self._mask = np.random.random(n) < np.repeat(self._train_fraction, n)
-        # self._idx = np.where(self._mask)
         # TODO: apply genes to X
         self._X = X
         self._y = y
-        self._model.fit(self._X[self._mask], self._y[self._mask], sample_weight, check_input, X_idx_sorted)
-        self._oob_score = self.predict(self._X[np.logical_not(self._mask)])
+        self._model.fit(self._X[self._mask], self._y[self._mask],
+                        sample_weight, check_input, X_idx_sorted)
+        self._oob_score = self.predict_proba(self._X[np.logical_not(self._mask)])[:,1]
         self._oob_truth = self._y[np.logical_not(self._mask)]
-        self._oob = np.power(self._oob_score - self._oob_truth, self._oob_power)
+        self._oob = self._oob_score - self._oob_truth
 
     def predict(self, X, check_input=True):
         predictions = self._model.predict(X, check_input=True)
-        # print(predictions.shape)
+        return predictions
+
+    def predict_proba(self, X, check_input=True):
+        predictions = self._model.predict_proba(X, check_input=True)
         return predictions
