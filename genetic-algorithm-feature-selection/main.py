4db39e118c2ec366623328a2b49290fe0204251d
maxjnorman <maxjnorman@gmail.com>
2020-09-13 13:15:39 +0100

    removed commented code

--- genetic-algorithm-feature-selection/main.py
+++ genetic-algorithm-feature-selection/main.py
@@ -9,57 +9,6 @@ from sklearn.metrics import roc_auc_score
 
 max_daughters = 8
 
-# clade = Clade(
-#     y="A",
-#     max_daughters=max_daughters,
-#     initial_descendants=[
-
-#         Individual(y=0),
-
-#         Individual(y=1),
-
-#         Clade(
-#             y="B",
-#             max_daughters=max_daughters,
-#             initial_descendants=[
-
-#                 Individual(y=2),
-
-#                 Individual(y=3),
-
-#                 Individual(y=4)
-#             ]),
-
-#         Clade(
-#             y="C",
-#             max_daughters=max_daughters,
-#             initial_descendants=[
-
-#                 Individual(y=5),
-
-#                 Clade(
-#                     y="D",
-#                     max_daughters=max_daughters,
-#                     initial_descendants=[
-
-#                         Individual(y=6),
-
-#                         Clade(
-#                             y="E",
-#                             max_daughters=max_daughters,
-#                             initial_descendants=[
-
-#                                 Individual(y=7),
-
-#                                 Individual(y=8),
-
-#                                 Individual(y=9)
-
-#                             ])
-#                     ])
-#             ])
-
-#     ])
 tree = DecisionTreeClassifier
 root = Clade(
     initial_descendants=[
@@ -78,22 +27,12 @@ root = Clade(
         Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3))],
     n_max=128, max_daughters=max_daughters, train_fraction=.75
     )
-# vine = Clade(
-#     initial_descendants=[Clade(
-#         initial_descendants=[Clade(
-#             initial_descendants=[Individual(model=tree(max_depth=3))]
-#             )]
-#         )]
-#     )
-
 
 dat = pd.read_csv("/Users/maxjnorman/OneDrive - HEOR Ltd/Projects/Main/HEOR00324 - ML INR/dataset_20/baseline_20_simple.csv")
 y = (dat["ttr_out_ros"] > 0.3).astype(int).values
 X = dat.loc[:,dat.columns != "ttr_out_ros"].values
 
 root.fit(X,y)
-
-
 
 train_fraction = .75
 _model = tree(max_depth=3)
@@ -113,5 +52,3 @@ _model.fit(_X[_mask], _y[_mask])
 _oob_score = _model.predict_proba(_X[np.logical_not(_mask)])[:,1]
 _oob_truth = _y[np.logical_not(_mask)]
 _oob = _oob_score - _oob_truth
-
-
