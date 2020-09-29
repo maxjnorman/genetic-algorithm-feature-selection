085bdb66c538fad6619a17a0319e69be35feeeb6
maxjnorman <maxjnorman@gmail.com>
2020-09-29 12:33:39 +0100

    new dataset

--- genetic-algorithm-feature-selection/main.py
+++ genetic-algorithm-feature-selection/main.py
@@ -6,60 +6,9 @@ from modules.clade import Clade, Individual
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import OneHotEncoder
 
 max_daughters = 8
-
-# clade = Clade(
-#     y="A",
-#     max_daughters=max_daughters,
-#     initial_descendants=[
-
-#         Individual(y=0),
-
-#         Individual(y=1),
-
-#         Clade(
-#             y="B",
-#             max_daughters=max_daughters,
-#             initial_descendants=[
-
-#                 Individual(y=2),
-
-#                 Individual(y=3),
-
-#                 Individual(y=4)
-#             ]),
-
-#         Clade(
-#             y="C",
-#             max_daughters=max_daughters,
-#             initial_descendants=[
-
-#                 Individual(y=5),
-
-#                 Clade(
-#                     y="D",
-#                     max_daughters=max_daughters,
-#                     initial_descendants=[
-
-#                         Individual(y=6),
-
-#                         Clade(
-#                             y="E",
-#                             max_daughters=max_daughters,
-#                             initial_descendants=[
-
-#                                 Individual(y=7),
-
-#                                 Individual(y=8),
-
-#                                 Individual(y=9)
-
-#                             ])
-#                     ])
-#             ])
-
-#     ])
 tree = DecisionTreeClassifier
 root = Clade(
     initial_descendants=[
@@ -78,40 +27,12 @@ root = Clade(
         Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3)),Individual(model=tree(max_depth=3))],
     n_max=128, max_daughters=max_daughters, train_fraction=.75
     )
-# vine = Clade(
-#     initial_descendants=[Clade(
-#         initial_descendants=[Clade(
-#             initial_descendants=[Individual(model=tree(max_depth=3))]
-#             )]
-#         )]
-#     )
 
-
-dat = pd.read_csv("/Users/maxjnorman/OneDrive - HEOR Ltd/Projects/Main/HEOR00324 - ML INR/dataset_20/baseline_20_simple.csv")
-y = (dat["ttr_out_ros"] > 0.3).astype(int).values
-X = dat.loc[:,dat.columns != "ttr_out_ros"].values
-
+dat = pd.read_csv(
+    "/home/max/projects/toy/ga-feat-selection/data/splice/Source/splice.data",
+    header=None
+)
+y = dat[0].str.match("^EI$").astype(int).values
+X = dat[2].str.strip().str.split('', expand=True).values
+X = OneHotEncoder().fit_transform(X)
 root.fit(X,y)
-
-
-
-train_fraction = .75
-_model = tree(max_depth=3)
-
-n = y.shape[0]
-_idx = np.random.choice(
-            np.arange(n),
-            int(np.ceil(n * train_fraction)),
-            replace=False
-        )
-mask = np.repeat(False, n)
-np.put(mask, _idx, True)
-_mask = mask
-_X = X
-_y = y
-_model.fit(_X[_mask], _y[_mask])
-_oob_score = _model.predict_proba(_X[np.logical_not(_mask)])[:,1]
-_oob_truth = _y[np.logical_not(_mask)]
-_oob = _oob_score - _oob_truth
-
-
